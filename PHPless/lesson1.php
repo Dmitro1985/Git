5c43c32462de23af63117ccc981c7380353f5df1
Dmitro1985 <Dima6@i.ua>
2016-06-08 21:57:10 +0300

    новая ветка
    учусь

--- PHPless/lesson1.php
+++ PHPless/lesson1.php
@@ -1,11 +1,3 @@
 <?php
 echo'hello';
 echo '<br>it is my first lessons';
-echo '<br>I like PHP';
-echo '<br>I like PHP';
-echo '<br>I like PHP';
-echo '<br>I like PHP';
-echo '<br>I like PHP';
-echo '<br>Wheare my cofe';
-echo '<br>Wheare my tae';
-echo '<br>Wheare my tae';
